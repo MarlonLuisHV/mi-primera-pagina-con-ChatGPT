b10ad61b676e4e22d0aae77e460c698d61dc38de
MarlonLuisHV <huaringavalverde16@gmail.com>
2024-03-13 20:51:34 -0500

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var ordenButton = document.getElementById('ordenButton');
+    
+    ordenButton.addEventListener('click', function() {
+      alert('¡Tu orden ha sido recibida! Pronto estará en camino.');
+    });
+  });
+  
